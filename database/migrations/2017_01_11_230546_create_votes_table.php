729c5f8e2b59d5d044b01f9dca599779fabace9c
vagrant <anton@inventiveit.se>
2017-01-11 23:45:34 +0000

    Added migration votes table

---
+++ database/migrations/2017_01_11_230546_create_votes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('votes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('issue_id')->index()->unsigned();
+            $table->integer('user_id')->index()->unsigned();
+            $table->timestamp('created_at');
+            $table->foreign('issue_id')->references('id')->on('issues')
+                ->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('votes');
+    }
+}
