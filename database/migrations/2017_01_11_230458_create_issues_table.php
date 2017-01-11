714ef9fdd638f91510ece12e1d7dfe7ce4ef2566
vagrant <anton@inventiveit.se>
2017-01-11 23:45:34 +0000

    Added migration issues table

---
+++ database/migrations/2017_01_11_230458_create_issues_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateIssuesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('issues', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->index()->unsigned();
+            $table->string('description');
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('issues');
+    }
+}
