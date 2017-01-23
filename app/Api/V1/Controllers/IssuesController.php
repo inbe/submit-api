e31f00c5bb32f568e31ef123740e27d6ec0b8d7c
inbe <anton@inventiveit.se>
2017-01-23 23:26:05 +0100

    Added usages, index and store methods

--- app/Api/V1/Controllers/IssuesController.php
+++ app/Api/V1/Controllers/IssuesController.php
@@ -2,10 +2,18 @@
 
 namespace App\Api\V1\Controllers;
 
+use JWTAuth;
+use App\Issue;
+use Dingo\Api\Routing\Helpers;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class IssuesController extends Controller
 {
+
+    use Helpers;
+
     /**
      * Display a listing of the resource.
      *
@@ -13,17 +21,9 @@ class IssuesController extends Controller
      */
     public function index()
     {
-        //
-    }
+      $issues = \App\Issue::all();
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+      return $issues->toArray();;
     }
 
     /**
@@ -34,7 +34,15 @@ class IssuesController extends Controller
      */
     public function store(Request $request)
     {
-        //
+      $issue = new Issue;
+      // TODO Verifi that user exists before creation (and is a good fella)
+      $issue->user_id = $request->get('user_id');
+      $issue->description = $request->get('description');
+
+      if($issue->save())
+          return $this->response->created();
+      else
+          return $this->response->error('could_not_create_issue', 500);
     }
 
     /**
