4426c3c2c05fffaacbeccd36444411c0a54ae17a
inbe <anton@inventiveit.se>
2017-01-23 22:15:38 +0100

    Added fields before foreign keys in issue tag table

--- database/migrations/2017_01_11_230635_create_issue_tag_table.php
+++ database/migrations/2017_01_11_230635_create_issue_tag_table.php
@@ -14,6 +14,9 @@ class CreateIssueTagTable extends Migration
     public function up()
     {
         Schema::create('issue_tag', function (Blueprint $table) {
+            $table->integer('issue_id')->index()->unsigned();
+            $table->integer('tag_id')->index()->unsigned();
+            $table->integer('user_id')->index()->unsigned();
             $table->foreign('issue_id')->references('id')->on('issues')
                 ->onUpdate('cascade')->onDelete('cascade');
             $table->foreign('tag_id')->references('id')->on('tags')
