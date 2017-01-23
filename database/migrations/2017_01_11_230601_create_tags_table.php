490e8e17392d4ebf4139305302b75934acd9819b
inbe <anton@inventiveit.se>
2017-01-23 22:14:12 +0100

    Reverted to usage of standard timestamp implementation

--- database/migrations/2017_01_11_230601_create_tags_table.php
+++ database/migrations/2017_01_11_230601_create_tags_table.php
@@ -16,7 +16,7 @@ class CreateTagsTable extends Migration
         Schema::create('tags', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name', 64);
-            $table->timestamp('created_at')->nullable();
+            $table->timestamps();
         });
     }
 
